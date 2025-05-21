730d244aac1daeaac4536376c86841d104757d86
Aryman Garcia Reis <arymanreis@gmail.com>
2025-05-21 19:30:47 -0300

    Avançando em handtracking

--- handtracking/detect_webcam.py
+++ handtracking/detect_webcam.py
@@ -13,34 +13,40 @@ resolution_y = 720
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
 
-def find_coord_hand(img):
+def find_coord_hand(img, side_inverted = False):
     img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(img_rgb)
     all_hands = []
     if result.multi_hand_landmarks:  # if hands are detected
-        for hand_landmarks in result.multi_hand_landmarks:
+        for hand_side, hand_landmarks in zip(result.multi_handedness, result.multi_hand_landmarks):
             hand_info = {}
             coords = []
             for mark in hand_landmarks.landmark:
-                coord_x = int(mark.x * resolution_x)
-                coord_y = int(mark.y * resolution_y)
-                coord_z = int(mark.z * resolution_x)
-                coords.append((coord_x, coord_y, coord_z))
+                coord_x, coord_y, coord_z = int(mark.x * resolution_x), int(mark.y * resolution_y), int(mark.z * resolution_x)
+                # print(coord_x, coord_y, coord_z)
+                coords.append((coord_x, coord_y, coord_z)) #Adiciona tupla dentro da lista
                 hand_info['coordenadas'] = coords
+                if side_inverted:
+                    if hand_side.classification[0].label == "Left":
+                        hand_info["side"] = "Right"
+                    else:
+                        hand_info["side"] = "Left"
+                else:
+                    hand_info["side"] = hand_side.classification[0].label
+                # print(hand_side)
+                # print(hand_side.classification[0].label)
+                print(hand_info["side"])
                 all_hands.append(hand_info)
-
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     return img, all_hands
 
-while camera.isOpened():
+while True:
     ret, frame = camera.read()
-
-    if not ret:
-        print("Frame vazio da câmera")
-        continue
+    # Inverte a imagem
+    frame = cv2.flip(frame, 1) # inverte esquerda pela direita
+    # img, all_hands = find_coord_hand(frame)
     img, all_hands = find_coord_hand(frame)
-    
-    cv2.imshow("Camera", frame)
+    cv2.imshow("Camera", img)
     key = cv2.waitKey(1)
     if key == 27:
         break
